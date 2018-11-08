7cf2695416017d45c639da4d7cec3c33a12b0064
Christian Westbrook <christianwestbrook@live.com>
2018-11-08 17:41:23 -0600

    Partial update to LogInFrame.java

--- qk-s-client/LogInFrame.java
+++ qk-s-client/LogInFrame.java
@@ -10,24 +10,23 @@ public class LogInFrame extends JFrame
 {
 	private int frameWidth = 800;
 	private int frameHeight = 480;
-	
+
 	private JLabel 			userLabel;
 	private JTextField 		userField;
 	private JLabel 			passLabel;
 	private JPasswordField 	passField;
 	private JButton 		loginButton;
-	
-	
+
+
 	private JPanel loginPanel;
-	
+
 	public LogInFrame()
 	{
-		userLabel 	= new JLabel("Username");
+		userLabel 	= new JLabel("Enter your name");
 		userField 	= new JTextField(10);
-		passLabel 	= new JLabel("Password");
-		passField 	= new JPasswordField(10);
 		loginButton = new JButton("Log In");
-		
+		loginButton.addActionEventListener(new LoginButtonListener());
+
 		// Create loginPanel
 		loginPanel = new JPanel();
 		loginPanel.add(userLabel);
@@ -35,10 +34,10 @@ public class LogInFrame extends JFrame
 		loginPanel.add(passLabel);
 		loginPanel.add(passField);
 		loginPanel.add(loginButton);
-		
+
 		// Add loginPanel to frame
 		this.add(loginPanel);
-		
+
 		// Configure frame
 		this.setMinimumSize(new Dimension(frameWidth, frameHeight));
 		this.setMaximumSize(new Dimension(frameWidth, frameHeight));
@@ -47,9 +46,17 @@ public class LogInFrame extends JFrame
 		this.setResizable(false);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setTitle("QuizKnows");
-		
+
 		// Display frame
 		this.pack();
 		this.setVisible(true);
+	}
+
+	private inner class LoginButtonListener implements EventListener
+	{
+		public void actionEvent(ActionEvent e)
+		{
+			
+		}
 	}
 }
